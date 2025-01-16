da0296eebee4925732a0a5641069bd04d8a5ef91
misiek <mbialozyt@student.agh.edu.pl>
2025-01-16 18:15:58 +0100

    add metric logging

--- src/main/java/org/flinkdemo/algorithms/EventAggregator.java
+++ src/main/java/org/flinkdemo/algorithms/EventAggregator.java
@@ -4,17 +4,23 @@ import static org.flinkdemo.outbound.PostgresUtil.eventAggregatorSink;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.metrics.Counter;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.flinkdemo.model.AmplitudeEvent;
 import org.flinkdemo.model.EventCount;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 public class EventAggregator {
 
     public static void performEventAggregation(
-        StreamExecutionEnvironment env, KafkaSource<AmplitudeEvent> kafkaSource, String postgresIp
+            StreamExecutionEnvironment env, KafkaSource<AmplitudeEvent> kafkaSource, String postgresIp
     ) throws Exception {
 
         var sourceStream = env.fromSource(
@@ -24,7 +30,7 @@ public class EventAggregator {
         );
 
         var aggregatedStream = sourceStream
-                .map(event -> new EventCount(event.getEventType(), 1L))
+                .map(new MetricEnrichedMapper())
                 .keyBy(EventCount::getEventType)
                 .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
                 .reduce(eventCountReduceFunction());
@@ -42,5 +48,36 @@ public class EventAggregator {
             t1.setCount(t1.getCount() + t2.getCount());
             return t1;
         };
+    }
+
+    public static class MetricEnrichedMapper extends RichMapFunction<AmplitudeEvent, EventCount> {
+
+        private transient Counter eventCounter;
+        private transient ScheduledExecutorService scheduler;
+
+        @Override
+        public void open(Configuration parameters) {
+            this.eventCounter = getRuntimeContext()
+                    .getMetricGroup()
+                    .counter("incoming_events_total");
+
+            this.scheduler = Executors.newSingleThreadScheduledExecutor();
+            this.scheduler.scheduleAtFixedRate(() -> {
+                System.out.println("Counter value: " + eventCounter.getCount());
+            }, 0, 10, TimeUnit.SECONDS);
+        }
+
+        @Override
+        public EventCount map(AmplitudeEvent event) {
+            this.eventCounter.inc();
+            return new EventCount(event.getEventType(), 1L);
+        }
+
+        @Override
+        public void close() {
+            if (scheduler != null && !scheduler.isShutdown()) {
+                scheduler.shutdown();
+            }
+        }
     }
 }
